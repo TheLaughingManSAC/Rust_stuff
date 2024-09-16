f0b89cd27c077b601947df36d5ed8c8f5a5b3d74
ArnasK <dessinger49@yahoo.com>
2024-09-16 12:12:39 +0100

    it does not run now

---
+++ hello_world/target/debug/deps/hello_world-d33ac290619cbfab.d
@@ -0,0 +1,5 @@
+C:\Users\dessi\OneDrive\Desktop\Mine\Rust_stuff\hello_world\target\debug\deps\libhello_world-d33ac290619cbfab.rmeta: src/main.rs
+
+C:\Users\dessi\OneDrive\Desktop\Mine\Rust_stuff\hello_world\target\debug\deps\hello_world-d33ac290619cbfab.d: src/main.rs
+
+src/main.rs:
